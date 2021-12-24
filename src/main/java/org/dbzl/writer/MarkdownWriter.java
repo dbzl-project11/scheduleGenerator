3fb9ac919ba95a2ca0d59cda2ade194d475feb8a
jordan <jranderson1990@yahoo.com>
2021-12-24 11:37:26 -0500

    add home game count to each schedule

--- src/main/java/org/dbzl/writer/MarkdownWriter.java
+++ src/main/java/org/dbzl/writer/MarkdownWriter.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +22,7 @@ public class MarkdownWriter {
             builder.append(getScheduleWeekHeader());
             matches.forEach(match -> builder.append(getScheduleMarkdown(match, matches.indexOf(match)+1)));
             builder.append('\n');
+
         });
         try{
             Files.write(Paths.get(filePath), builder.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
@@ -75,12 +75,13 @@ public class MarkdownWriter {
             }
             builder.append("| ").append(match.getHomeTeam().getName()).append(" | ").append(match.getAwayTeam().getName()).append(" |").append('\n');
         }
-
+        builder.append("Home game count: ").append(team.getHomeGamesCount()).append('\n');
+        builder.append('\n');
         return builder.toString();
     }
 
 
-        public String getScheduleMarkdown(Match match, int week){
+    public String getScheduleMarkdown(Match match, int week){
         StringBuilder builder = new StringBuilder(200);
         builder.append("|").append(week);
         if(match.isDivisionalMatch()){
